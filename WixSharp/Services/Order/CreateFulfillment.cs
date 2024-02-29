dcc93391c54a45fec9f4e9f6bca53fa768204f2c
jisha <jisha@primepenguin.com>
2024-02-29 14:08:06 +0530

    Fulfillment entity changes

---
+++ WixSharp/Services/Order/CreateFulfillment.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace WixSharp.Services.Order
+{
+    public class CreateFulFillment
+    {
+        [JsonProperty("fulfillment")]
+        public FulfillmentInput Fulfillment { get; set; }
+    }
+
+    public  class FulfillmentInput
+    {
+        [JsonProperty("lineItems")]
+        public LineItemInput[] LineItems { get; set; }
+
+        [JsonProperty("trackingInfo")]
+        public FulfillmentTrackingInfo TrackingInfo { get; set; }
+    }
+
+    public  class LineItemInput
+    {
+        [JsonProperty("index")]
+        public long Index { get; set; }
+
+        [JsonProperty("quantity")]
+        public long Quantity { get; set; }
+    }
+}
